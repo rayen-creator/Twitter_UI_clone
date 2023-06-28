f22091bedaf498e0db2c44d0dd9cb52fae6775d0
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-06-28 14:28:15 +0100

    💄 clean the app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-
+import Rightbar from './components/rightbar'
 import Siderbar from './components/siderbar'
 
 function App() {
 
   return (
-   <Siderbar></Siderbar>
-  
+    <div className="h-screen flex flex-row divide-x">
+      <Siderbar />
+      <div className='flex-1'></div>
+      <Rightbar />
+    </div>
   )
 }
 
